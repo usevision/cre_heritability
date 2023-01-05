97b2a25cf0612fbf519be7b28342b50939fbca9a
Kate Weaver <kweave23@jhu.edu>
2023-01-05 10:49:35 -0500

    update with file wget

--- extract_clusters.sh
+++ extract_clusters.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-#wget --timestamping 'ftp://hgdownload.soe.ucsc.edu/goldenPath/hg38/liftOver/hg38ToHg19.over.chain.gz' -O ~/scr4_rmccoy22/kweave23/resources/hg38toHg19.over.chain.gz
+wget --timestamping 'ftp://hgdownload.soe.ucsc.edu/goldenPath/hg38/liftOver/hg38ToHg19.over.chain.gz' -O ~/scr4_rmccoy22/kweave23/resources/hg38toHg19.over.chain.gz
+
+wget --no-check-certificate "https://drive.google.com/uc?export=download&id=1V8makOyvdCPUKv07e5YLax3nX5noDI8s" -O 'S3V2_IDEAS_hg38_ccre2.cCRE.M.notall0.rmallNEU.withid.clusterID.JclusterID.bed'
 
 ml anaconda
-#conda create -c bioconda -n liftover ucsc-liftover -y
+conda create -c bioconda -n liftover ucsc-liftover -y
 conda activate liftover
 
-awk 'BEGIN { OFS = "_" } ; NR==FNR{a[$1, $2, $3]; next} !(($1, $2, $3) in a)' ../IDEAS_sept/S3V2_IDEAS_hg38_ccre2.cCRE.M.notall0.rmallNEU.hg38.failed.nocomments.bed S3V2_IDEAS_hg38_ccre2.cCRE.M.notall0.rmallNEU.withid.clusterID.JclusterID.bed > metaclusters_withoutFailedLiftover.txt 
+awk 'BEGIN { OFS = "_" } ; NR==FNR{a[$1, $2, $3]; next} !(($1, $2, $3) in a)' ../IDEAS_sept/S3V2_IDEAS_hg38_ccre2.cCRE.M.notall0.rmallNEU.hg38.failed.nocomments.bed S3V2_IDEAS_hg38_ccre2.cCRE.M.notall0.rmallNEU.withid.clusterID.JclusterID.bed > metaclusters_withoutFailedLiftover.txt
 
 for clusternum in {1..15}
 do
